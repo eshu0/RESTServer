2d1e6b129de62d6e501d199422873a1780ad44e9
eshu0 <willsawesometoolkit@gmail.com>
2020-05-30 12:55:51 +0100

    seperated into seperate files

---
+++ pkg/server/handlers.go
@@ -0,0 +1,106 @@
+package RESTServer
+
+import (
+	"context"
+	"net/http"
+	"reflect"
+	"html/template"
+	"errors"
+    "io/ioutil"
+	"strings"
+
+	Helpers "github.com/eshu0/RESTServer/pkg/helpers"
+	Handlers "github.com/eshu0/RESTServer/pkg/handlers"
+	Config "github.com/eshu0/RESTServer/pkg/config"
+
+	sl "github.com/eshu0/simplelogger"
+	sli "github.com/eshu0/simplelogger/interfaces"
+	mux "github.com/gorilla/mux"
+)
+
+func (rs *RServer) MakeHandlerFunction(MethodName string, any interface{}) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rs.Invoke(any, MethodName, w, r)
+	}
+}
+
+func (rs *RServer) AddStaticHandler(URL string, StaticDir string)  {
+	rs.Config.AddHandler(rs.CreateStaticHandler(URL,StaticDir))
+}
+
+func (rs *RServer) AddFunctionHandler(URL string, MethodName string,HTTPMethod string, FunctionalClass string)  {
+	rs.Config.AddHandler(rs.CreateFunctionHandler(URL,MethodName,HTTPMethod,FunctionalClass))
+}
+
+func (rs *RServer) CreateStaticHandler(URL string, StaticDir string) Handlers.RESTHandler {
+	drhr := Handlers.RESTHandler{}
+	drhr.URL = URL
+	drhr.StaticDir = StaticDir
+	return drhr
+}
+
+func (rs *RServer) CreateFunctionHandler(URL string, MethodName string,HTTPMethod string, FunctionalClass string) Handlers.RESTHandler {
+	drhr := Handlers.RESTHandler{}
+	drhr.URL = URL
+	drhr.MethodName = MethodName
+	drhr.HTTPMethod = HTTPMethod
+	drhr.FunctionalClass = FunctionalClass
+	return drhr
+}
+
+func (rs *RServer) addHandlerToRouter(r *mux.Router, handl Handlers.RESTHandler){
+
+	funcclass, ok := rs.FunctionalMap[handl.FunctionalClass]
+
+	if ok {
+		if handl.TemplatePath != "" || handl.TemplateFileName != "" || handl.TemplateBlob != ""  {
+			rs.Log.LogDebugf("addHandlertoRouter", "Handlers: Adding Template function %s", handl.MethodName)
+			
+			if handl.HTTPMethod != ""{
+				if strings.Contains(handl.HTTPMethod,",") {
+					r.HandleFunc(handl.URL, rs.MakeTemplateHandlerFunction(handl, funcclass)).Methods(strings.Split(handl.HTTPMethod,",")...)
+
+				}else{
+					r.HandleFunc(handl.URL, rs.MakeTemplateHandlerFunction(handl, funcclass)).Methods(handl.HTTPMethod)
+				}
+			}else{
+				r.HandleFunc(handl.URL, rs.MakeTemplateHandlerFunction(handl, funcclass))
+			}
+		} else {
+			rs.Log.LogDebugf("addHandlertoRouter", "Handlers: Adding %s", handl.MethodName)
+			if handl.HTTPMethod != ""{
+				if strings.Contains(handl.HTTPMethod,",") {
+					r.HandleFunc(handl.URL, rs.MakeHandlerFunction(handl.MethodName, funcclass)).Methods(strings.Split(handl.HTTPMethod,",")...)
+
+				}else{
+					r.HandleFunc(handl.URL, rs.MakeHandlerFunction(handl.MethodName, funcclass)).Methods(handl.HTTPMethod)
+				}
+			}else{
+				r.HandleFunc(handl.URL, rs.MakeHandlerFunction(handl.MethodName, funcclass))
+			}
+		}
+	} else {
+		if handl.StaticDir != "" {
+			rs.Log.LogDebugf("addHandlertoRouter", "Handlers: Adding route %s for  static directory %s", handl.URL, handl.StaticDir)		
+			r.PathPrefix(handl.URL).Handler(http.StripPrefix(handl.URL, http.FileServer(http.Dir(handl.StaticDir))))
+
+		} else {
+			rs.Log.LogError("addHandlertoRouter", "Handlers Error FunctionalClass (%s) doesn't have a function mapped", handl.FunctionalClass)		
+		}
+	}
+}
+
+func (rs *RServer) MapFunctionsToHandlers() *mux.Router {
+
+	r := mux.NewRouter()
+
+	for _, handl := range rs.Config.GetHandlers() {
+		rs.addHandlerToRouter(r,handl)
+	}
+
+	for _, handl := range rs.Config.GetDefaultHandlers() {
+		rs.addHandlerToRouter(r,handl)
+	}
+
+	return r
+}
---
+++ pkg/server/templates.go
@@ -0,0 +1,147 @@
+package RESTServer
+
+import (
+	"context"
+	"net/http"
+	"reflect"
+	"html/template"
+	"errors"
+    "io/ioutil"
+	"strings"
+
+	Helpers "github.com/eshu0/RESTServer/pkg/helpers"
+	Handlers "github.com/eshu0/RESTServer/pkg/handlers"
+	Config "github.com/eshu0/RESTServer/pkg/config"
+
+	sl "github.com/eshu0/simplelogger"
+	sli "github.com/eshu0/simplelogger/interfaces"
+	mux "github.com/gorilla/mux"
+)
+
+
+func (rs *RServer) MakeTemplateHandlerFunction(handler Handlers.RESTHandler, any interface{}) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		rs.Log.LogDebug("MakeTemplateHandlerFunction", "MakeTemplateHandlerFunction called")
+		
+		if rs.Config.GetCacheTemplates(){
+			rs.Log.LogDebugf("MakeTemplateHandlerFunction", "Looking up template %s for %s ",handler.TemplateName,handler.URL)
+			t := rs.Templates.Lookup(handler.TemplateName) 
+			rs.Invoke(any, handler.MethodName, w, r, t)
+		} else {
+			t := template.New(handler.TemplateName) 
+			err := errors.New("should not see this error")
+			
+			if handler.TemplatePath != "" {
+				rs.Log.LogDebugf("MakeTemplateHandlerFunction", "We have a template path %s for %s", handler.TemplatePath,handler.URL)
+				t, err = template.ParseFiles(handler.TemplatePath)
+			} else {
+				if handler.TemplateFileName != "" {
+					tfilepath := rs.Config.GetTemplatePath() + handler.TemplateFileName 
+					rs.Log.LogDebugf("MakeTemplateHandlerFunction", "We have a template filename %s for %s", tfilepath,handler.URL)
+					t, err = template.ParseFiles(tfilepath)
+				} else {
+					if handler.TemplateBlob != "" {
+						rs.Log.LogDebugf("MakeTemplateHandlerFunction", "We have a template blob %s for %s", handler.TemplateBlob,handler.URL)
+						t, err = template.New(handler.TemplateName).Parse(handler.TemplateBlob)
+					} else {
+						err = errors.New("No template set")
+					}
+				}
+			}
+		
+			if err != nil {
+				rs.Log.LogErrorf("MakeTemplateHandlerFunction", "Error : %s", err.Error())
+				return
+			}
+			rs.Invoke(any, handler.MethodName, w, r, t)
+		}
+	
+	}
+
+}
+
+func (rs *RServer) AddTemplateHandler(URL string, MethodName string,HTTPMethod string, FunctionalClass string, TemplateName string, Filename string)  {
+	rs.AddTemplateHandlerWithBlob(URL,MethodName,HTTPMethod,FunctionalClass,TemplateName,"",Filename)
+}
+
+func (rs *RServer) AddTemplateHandlerWithBlob(URL string, MethodName string,HTTPMethod string, FunctionalClass string, TemplateName string, Blob string, Filename string)  {
+	rs.Config.AddHandler(rs.CreateTemplateHandler(URL,MethodName,HTTPMethod,FunctionalClass,TemplateName,Blob,Filename))
+}
+
+func (rs *RServer) AddBlobTemplateHandler(URL string, MethodName string,HTTPMethod string, FunctionalClass string, TemplateName string, Blob string, Path string)  {
+	rs.Config.AddHandler(rs.CreateBlobTemplateHandler(URL,MethodName,HTTPMethod,FunctionalClass,TemplateName,Blob,Path))
+}
+
+func (rs *RServer) AddSpecificTemplateHandler(URL string, MethodName string,HTTPMethod string, FunctionalClass string, TemplateName string, Blob string, Filename string)  {
+	rs.Config.AddHandler(rs.CreateSpecificTemplateHandler(URL,MethodName,HTTPMethod,FunctionalClass,TemplateName,Blob,Filename))
+}
+
+
+func (rs *RServer) CreateTemplateHandler(URL string, MethodName string,HTTPMethod string, FunctionalClass string, TemplateName string, Blob string, Filename string) Handlers.RESTHandler {
+	drhr := rs.CreateFunctionHandler(URL, MethodName, HTTPMethod, FunctionalClass)
+	drhr.TemplateBlob = Blob
+	drhr.TemplateFileName = Filename
+	drhr.TemplateName = TemplateName		
+	return drhr
+}
+
+func (rs *RServer) CreateBlobTemplateHandler(URL string, MethodName string,HTTPMethod string, FunctionalClass string, TemplateName string, Blob string, Path string) Handlers.RESTHandler {
+	drhr := rs.CreateFunctionHandler(URL, MethodName, HTTPMethod, FunctionalClass)
+	drhr.TemplateBlob = Blob
+	drhr.TemplatePath = Path
+	drhr.TemplateName = TemplateName		
+	return drhr
+}
+
+func (rs *RServer) CreateSpecificTemplateHandler(URL string, MethodName string,HTTPMethod string, FunctionalClass string, TemplateName string, Blob string, Path string) Handlers.RESTHandler {
+	drhr := rs.CreateFunctionHandler(URL, MethodName, HTTPMethod, FunctionalClass)
+	drhr.TemplateBlob = Blob
+	drhr.TemplatePath = Path
+	drhr.TemplateName = TemplateName		
+	return drhr
+}
+
+func (rs *RServer) LoadTemplates(){
+
+	if rs.Config.HasTemplate() && rs.Config.GetCacheTemplates() {
+
+		var allFiles []string
+		TemplatePath := rs.Config.GetTemplatePath() 
+		files, err := ioutil.ReadDir(TemplatePath)
+		if err != nil {
+			rs.Templates = nil
+			rs.Log.LogErrorf("LoadTemplates", "ReadDir - Error : %s", err.Error())
+			return 
+		}
+	
+		filetypes := rs.Config.GetTemplateFileTypes()
+		for _, file := range files {
+			filename := file.Name()
+			for _, filetype := range filetypes {
+				if strings.HasSuffix(filename, filetype) {
+					allFiles = append(allFiles, TemplatePath+filename)
+				}
+			}
+		}
+	
+		templates, terr := template.ParseFiles(allFiles...) 
+		if terr != nil {
+			rs.Templates = nil
+			rs.Log.LogErrorf("LoadTemplates", "ParseFiles - Error : %s", terr.Error())
+			return 
+		}
+		rs.Log.LogDebug("LoadTemplates", "Loaded Templates")
+		rs.Templates = templates
+	} else {
+		if !rs.Config.HasTemplate() {
+			rs.Log.LogDebug("LoadTemplates", "No Template")
+		}
+
+		if !rs.Config.GetCacheTemplates() {
+			rs.Log.LogDebug("LoadTemplates", "Not Caching Templates")
+		}
+
+		rs.Templates = nil
+	}
+}
