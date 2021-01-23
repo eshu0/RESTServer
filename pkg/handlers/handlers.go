673edbb6faa6f0e9dc1e74832c6e5f861236c2a6
eshu0 <willsawesometoolkit@gmail.com>
2021-01-23 09:07:35 +0000

    omitempty

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,21 +1,22 @@
 package RESTHandlers
 
+//RESTHandler is the struct that represents a REST call
 type RESTHandler struct {
-	URL             string `json:"url"`
-	MethodName      string `json:"methodname"`
-	HTTPMethod      string `json:"httpmethod"`
-	FunctionalClass string `json:"functionalclass"`
+	URL             string `json:"url,omitempty"`
+	MethodName      string `json:"methodname,omitempty""`
+	HTTPMethod      string `json:"httpmethod,omitempty""`
+	FunctionalClass string `json:"functionalclass,omitempty""`
 	// Static file handling
-	StaticDir string `json:"staticdir"`
+	StaticDir string `json:"staticdir,omitempty""`
 	// Template details
-	TemplatePath     string `json:"templatepath"`
-	TemplateFileName string `json:"templatefilename"`
-	TemplateBlob     string `json:"templateblob"`
-	TemplateName     string `json:"templatename"`
+	TemplatePath     string `json:"templatepath,omitempty""`
+	TemplateFileName string `json:"templatefilename,omitempty""`
+	TemplateBlob     string `json:"templateblob,omitempty""`
+	TemplateName     string `json:"templatename,omitempty""`
 	//JSON Handling
-	JSONRequest      bool        `json:"jsonrequest"`
-	JSONResponse     bool        `json:"jsonresponse"`
+	JSONRequest      bool        `json:"jsonrequest,omitempty""`
+	JSONResponse     bool        `json:"jsonresponse,omitempty""`
 	JSONRequestType  interface{} `json:"-"`
-	JSONRequestData  string      `json:"jsonin"`
-	JSONResponseData string      `json:"jsonout"`
+	JSONRequestData  string      `json:"jsonin,omitempty""`
+	JSONResponseData string      `json:"jsonout,omitempty""`
 }
