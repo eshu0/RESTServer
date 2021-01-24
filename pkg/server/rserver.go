a6c5a0e0a8266910fb1e54619d99250e6fc66d46
eshu0 <willsawesometoolkit@gmail.com>
2021-01-24 18:37:20 +0000

    Update rserver.go

--- pkg/server/rserver.go
+++ pkg/server/rserver.go
@@ -178,7 +178,7 @@ func (rs *RServer) LoadConfig() bool {
 		return false
 	}
 
-	rs.LogDebugf("LoadConfig", "Updated from file path %s", rs.Config.Helper.Filepath)
+	rs.LogDebugf("LoadConfig", "Updated from file path %s", rs.Config.Helper.FilePath)
 	return true
 }
 
