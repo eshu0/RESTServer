ba4dabee175f344563b94aa6cc06b15fcf08fac0
eshu0 <willsawesometoolkit@gmail.com>
2021-01-24 14:48:54 +0000

    Update main.go

--- examples/customlisttemplate/main.go
+++ examples/customlisttemplate/main.go
@@ -19,7 +19,7 @@ func main() {
 	RESTCommands.SetDefaultFunctionalMap(server)
 
 	// as a test save the updated config
-	server.ConfigFilePath = "./updated.json"
+	server.Config.ConfigFilePath = "./updated.json"
 	server.SaveConfig()
 
 	// start Listen Server, this build the mapping and creates Handler/
